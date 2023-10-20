4a060576b0b0842d355288b56e1fb89ea03bffdd
Anderson Gonzalez <andersongonzalez2000@gmail.com>
2023-10-20 17:38:31 -0500

    Rutas establecidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+import { CountriesRoutingModule } from './countries/countries-routing.module';
+import { CountriesModule } from './countries/countries.module';
 
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomePageComponent
   },
   {
@@ -14,8 +17,16 @@ const routes: Routes = [
     component: AboutPageComponent
   },
   {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule),
+  },
+  {
     path: '**',
-    redirectTo: 'home'
+    redirectTo: ''
   },
 ];
 @NgModule({
